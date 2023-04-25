d3290d7d5ab73edbf06c2476e1f50c9bf26272ca
YCN-AFS <70703361+YCN-AFS@users.noreply.github.com>
2023-04-25 23:06:46 +0700

    test by github

---
+++ function.js
@@ -0,0 +1,5 @@
+alert("Cách đây gần 20 năm có một thiên thạch khổng lồ rơi xuống Trái Đất và "bùm": Thế là bạn Ánh ra đời. 🎂 🎶🎉🎊");
+window.addEventListener('click', function() {
+    var audio = document.getElementById("HBD");
+    audio.play();
+});
